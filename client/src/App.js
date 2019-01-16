12fdd67bd92a9043064e0c7c46842c0735f58c11
Brad Kaufman <brad@thoughtive.io>
2019-01-16 17:28:08 -0500

    Adding table from EnhancedTable.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,45 @@
 import React, {Component} from 'react';
 import './App.css';
 import Typography from "@material-ui/core/Typography";
-import Table from './Table';
+import Paper from "@material-ui/core/Paper";
+import EnhancedTable from './EnhancedTable';
 import Dashboard from "./dashboard";
+import SimpleTable from "./SimpleTable";
+
+function createData(fullname, username, email, orgId) {
+    return { fullname, username, email, orgId };
+};
 
 
 class App extends Component {
     state = {users: []};
 
+
     componentDidMount() {
         fetch('/api/person')
             .then(res => res.json())
             .then(users => this.setState({users}));
-    }
+    };
+
+
+
 
     render() {
         return (
             <div className="App">
                 <Typography component="div">
-                    <Dashboard/>
-
                     <h1>Users</h1>
                     {this.state.users.map(user =>
-                        <div key={user.id}>{user.username}</div>
+                        <div key={user.id}>{user.username}, {user.fullName}</div>
                     )}
 
-                    <Table
-                        data={this.state.users}
-                        header={[
-                            {
-                                name: "Full Name",
-                                prop: "fullname"
-                            },
-                            {
-                                name: "Username",
-                                prop: "username"
-                            },
-                            {
-                                name: "Email",
-                                prop: "email"
-                            },
-                            {
-                                name: "Org ID",
-                                prop: "orgId"
-                            }
-                        ]}
-                    />
+
+                    <EnhancedTable />
+
                 </Typography>
             </div>);
     }
-}
 
+}
 export default App;
+

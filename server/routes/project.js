108663f7c17704df913a80395cc748d7b1f700dd
Brad Kaufman <brad@thoughtive.io>
2019-01-27 18:19:38 -0500

    minor chnages

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,9 +1,9 @@
 const projectController = require('../controllers').project;
 
 module.exports = (app) => {
-    // app.get('/api', (req, res) => res.status(200).send({
-    //     message: 'Welcome to the project API!',
-    // }));
+     app.get('/api', (req, res) => res.status(200).send({
+        message: 'Welcome to the project API!',
+     }));
 
     // Create a project
     app.post('/api/project', projectController.create);

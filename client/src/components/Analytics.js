fd9e7793c55d776eccc39d7aa8e4d5640cfe688d
Brad Kaufman <brad@thoughtive.io>
2019-04-05 10:17:33 -0400

    Superset

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -2,25 +2,23 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Topbar from "./Topbar";
-import Frame from 'react-frame-component';
-import { styles } from "./styles/MaterialSense";
+import zoid from "zoid";
 
-const supersetUrl = "http://ec2-13-59-69-6.us-east-2.compute.amazonaws.com:8088";
+const SupersetZoidComponent = zoid.create({
+  // The html tag used to render my component
+  tag: "superset-component",
+  // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+  url: "http://ec2-13-59-69-6.us-east-2.compute.amazonaws.com:8088"
+});
 
 class Analytics extends Component {
 
-
   render() {
-    const { classes } = this.props;
-    const currentPath = this.props.location.pathname;
-
     return (
       <React.Fragment>
         <CssBaseline />
         <Topbar currentPath={currentPath} />
-        <Frame>
-          { supersetUrl }
-        </Frame>
+        <SupersetZoidComponent />
       </React.Fragment>
     );
   }
